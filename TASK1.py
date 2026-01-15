8ed90d55379bee5c05ddfd63ea487752253d2de0
shreyamishra4379 <shreyamishra7410@gmail.com>
2026-01-15 20:04:54 +0530

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,20 @@
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the second number:"))
+
+
+Addition = n1+n2
+subtraction =n1-n2
+multiplcation = n1*n2
+division =n1/n2
+
+print("Addition:",Addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplcation)
+print("Division:",division)
+
+#Enter the first number:5
+#Enter the second number:10
+#Addition: 15
+#Subtraction: -5
+#Multiplication: 50
+#Division: 0.5
---
+++ Task2.py
@@ -0,0 +1,9 @@
+n1=input("Enter your  first name:")
+n2=input("Enter your last name:")
+
+full_name = n1 + " " + n2
+print("Hello", full_name ,"!","Welcome to the Python program.")
+
+#Enter your  first name:himanshu
+#Enter your last name:mishra
+#Hello himanshu mishra ! Welcome to the Python program.
